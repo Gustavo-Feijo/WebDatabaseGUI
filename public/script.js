b270c24500d881341499d3178c078b8cee76df51
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2024-01-19 19:48:37 -0300

    Added Jquery terminal, improved visual, added server post methods

--- public/script.js
+++ public/script.js
@@ -1,14 +1,19 @@
-$(document).ready(function () {
-    $('#create_db span').click(function (e) {
-        e.preventDefault();
-        $('#create_db div').slideToggle();
+$(function () {
 
+    //Makes the input field to the name of the new database 
+    $('#create_db span').on('click', function (e) {
+        $('#create_db div').slideToggle();
     });
-    $('#db_confirm').click(function (e) {
+    $('#db_confirm').on('click', function (e) {
         let input = $('#db_name_input').val();
         var database = $('<div>').text(input).addClass('button database');
 
         $('#db_container').append(database.hide().fadeIn(1000));
         $('#create_db div').slideToggle();
+    });
+    var term = $('#terminal').terminal(function (command) {
+        console.log(typeof command);
+    }, {
+        greetings: 'Welcome to the terminal, insert your query.'
     });
 });
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
-
+import fs from 'fs';
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import DatabaseOperations from './database/db_operations.js';
 const app = express();
 
 //Sets the APP to serve the public directory.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
 //Sends the app html file when entering the site.
-app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname, 'public','app.html'));
+app.get('/', (req, res) => {
+    res.sendFile('./public/app.html');
 });
+
+app.post('/api/query', async (req, res) => {
+    try {
+        const query = req.body.query;
+        const DB = new DatabaseOperations();
+        const result = await DB.runQuery(query);
+        res.status(200).json(result);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'INTERNAL SERVER ERROR' });
+    }
+}
+);
 
 app.listen(5500);
