8baf7f6335a995d922c911577c05c2a6910b7bed
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2024-01-21 14:45:07 -0300

    Created the MySQL connection and the operations to run a query and create a new database

--- database/db_connect.js
+++ database/db_connect.js
@@ -2,15 +2,19 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config({ path: './database/credentials.env' });
 
-let connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+//Asynchronous function that, when called, creates a mysql connection and returns it.
+async function createConnection(databaseName) {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: databaseName,
+        });
+        return connection;
+    } catch (error) {
+        throw error;
+    }
+}
 
-// Listen for the 'error' event
-connection.on('error', (err) => {
-    console.error('MySQL connection error:', err.message);
-});
-
-export default connection;
+export default createConnection;
--- database/db_operations.js
+++ database/db_operations.js
@@ -1,15 +1,42 @@
-import connection from "./db_connect.js";
+import createConnection from "./db_connect.js";
 
-class DatabaseOperations {
-    async runQuery(query) {
+//Class with the methods to run queries on the database.
+class databaseOperations {
+
+    //Most simple method, receive a query string and a optional database name.
+    async runQuery(query, databaseName = null) {
+
         try {
+            //Creates the connection. If there is a database selected on the front, runs the query on there.
+            const connection = await createConnection(databaseName);
+
+            //Executes the query on the connection.
             const [result, fields] = await connection.execute(query);
+            connection.end();
+
+            //Returns the result of the query to the server.
+            return result;
+
+        } catch (err) {
+            //If there is any error, throws it to the server call.
+            throw err.message;
+        }
+    }
+
+    //Method that creates a new database with the specified name.
+    async createDatabase(databaseName) {
+        try {
+            //Creates a new connection without a specified database and creates one if it doesn't exist.
+            const connection = await createConnection();
+            console.log(databaseName);
+            const [result, fields] = await connection.execute(`CREATE DATABASE ${databaseName}`);
+            connection.end();
+
             return result;
         } catch (err) {
-            console.error('Error executing query:', err.message);
-            throw err; // Rethrow the error to handle it in the calling code
+            throw err.message;
         }
     }
 }
 
-export default DatabaseOperations;
+export default databaseOperations;
